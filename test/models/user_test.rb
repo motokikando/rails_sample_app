1361abf1db68ce9fcc5e58abde7afcc1e31986a6
motokikando <kando813motoki@gmail.com>
2022-07-07 00:20:10 +0900

    add email_regular expression test case

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(name: "ExampleUser", email: "hoge@toki.waseda.jp" )
+    @user = User.new(name: "ExampleUser", email: "hoge@exaple.com" )
   end
 
   test "should be valid " do
@@ -32,4 +32,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
 end
