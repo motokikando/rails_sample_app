1b4e25a2d701518ba8e3f0ce910a500ad63fc24f
motokikando <kando813motoki@gmail.com>
2022-07-08 03:02:04 +0900

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,8 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
-  def create_table
-    @user = User.new(params[:user])
+  def create
+    @user = User.new(user_params)
     if @user.save
       #保存の成功
       puts "成功"
@@ -20,4 +20,5 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
+
 end
