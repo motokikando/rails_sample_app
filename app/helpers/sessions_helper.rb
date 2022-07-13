25f6b03757f5677a43b25ed2589cc6a7240a5dd9
motokikando <kando813motoki@gmail.com>
2022-07-13 18:06:58 +0900

    add forget token method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -22,7 +22,7 @@ module SessionsHelper
                 log_in user
                 @current_user = user
             end
-        end 
+        end
     end
 
     #ユーザーがログインしていればtrue, その他ならfalseを返す
@@ -30,7 +30,16 @@ module SessionsHelper
         !current_user.nil?
     end
 
+    #永続的セッションを破棄する
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
+
+    #現在のユーザーをログアウトする
     def log_out
+        forget(current_user)
         session.delete(:user_id)
         @current_user = nil
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,30 @@ class User < ApplicationRecord
     has_secure_password
     validates :password, presence: true, length: { minimum:6 }
 
+    # 渡された文字列のハッシュ値を返す
     def self.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
     end
 
+    #ランダムなトークンを返す
     def self.new_token
         SecureRandom.urlsafe_base64
     end
 
+    #永続セッションのためにユーザーをデータベースに記憶する
     def remember
         self.remember_token = User.new_token
         update_attribute(:remember_digest, User.new_token)
     end
 
+    #渡されたトークンがダイジェストと一致したらtrueを返す
     def authenticate?(remember_token)
         BCrypt::Password.new(remember_digest).is_password?(remember_token)
-    end 
+    end
+
+    # ユーザーのログイン情報を破棄する
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
