0541fae1f14b9fb4f1f4313d50e4f6f6823dc74c
motokikando <kando813motoki@gmail.com>
2022-07-14 19:00:16 +0900

    add remember me session check box

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user#リダイレクト
     else
       #エラーメッセージ
@@ -13,6 +14,7 @@ class SessionsController < ApplicationController
       render 'new'
     end
   end
+
   def destroy
     log_out if logged_in?
     redirect_to root_url
