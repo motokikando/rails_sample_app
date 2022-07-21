fc87e5cc2da05cc10b1ab48480d9dd6a4290e375
motokikando <kando813motoki@gmail.com>
2022-07-21 16:34:36 +0900

    add user_mailer

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,26 @@
+class UserMailer < ApplicationMailer
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.account_activation.subject
+  #
+  def account_activation(user)
+    #@userに引数のuserを格納
+    @user = user
+    #mail to: メソッドで、　第一引数に user.email, 第二引数に subjekt: "Account activation" を代入する
+    @greeting = "Hi"
+    mail to: user.email, subject: "Account activation"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.password_reset.subject
+  #
+  def password_reset
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
