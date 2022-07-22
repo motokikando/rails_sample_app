b7008848c071bdc52a0559aa0795027fc6fbacd4
motokikando <kando813motoki@gmail.com>
2022-07-22 20:30:15 +0900

    add

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,6 +3,7 @@ class AccountActivationsController < ApplicationController
     def edit
         user = User.find_by(email: params[:email])
         if user && !user.activated? && user.authenticated?(:activation, params[:id])
+            user.activate
             user.update_attribute(:activated, true)
             user.update_attribute(:activated_at, Time.zone.now)
             log_in user
@@ -11,6 +12,6 @@ class AccountActivationsController < ApplicationController
         else
             flash[:danger] = "invalid activation link"
             redirect_to root_url
-        end 
+        end
     end
 end
